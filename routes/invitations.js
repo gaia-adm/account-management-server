bdda44bdd2106be84061cd402a604a63f8720338
boriska70@gmail.com <boriska70@gmail.com>
2016-11-09 11:45:49 +0200

    Trying to fix

--- routes/invitations.js
+++ routes/invitations.js
@@ -155,6 +155,7 @@ router.route('/return.google')
   .get(
       function (req) {
         console.log('BORIS invitations-156: ' + req.protocol + '://' + req.get('host') + req.originalUrl);
+          next();
       },
     passport.authenticate('google-invitation', {
       session: false,
