a3f64be69b4689b3ca4d5ea7be8a72eb46c12e8a
boriska70@gmail.com <boriska70@gmail.com>
2016-11-15 21:23:00 +0200

    cleanup

--- TipsAndTricks.md
+++ TipsAndTricks.md
@@ -18,3 +18,6 @@ There are 2 files to debug or print details:
  
 #### Re-seed the database
  - predefined users will be re-created when executing seed:run --production. During this process their assignment to any account will be removed. So do not assign them to any account, if needed create a additional (temporary or not) user and assign it to the account. 
+ 
+#### LIMITATIONS:
+- **Vagrant only**: since our vagrant (.local) domain is not publicly accessible as Google Oauth2 requires and we don't use "localhost" as a hostname in order to handle cookies properly, invitation process is broken on vagrant. Once you receive "The site cannot be reached" error for the URL ending with /return.google, you need to replace localhost with acmc.gaia-local.skydns.local manually in the browser and refresh the page
--- seeds/production/1_production.js
+++ seeds/production/1_production.js
@@ -3,8 +3,7 @@ exports.seed = function(knex, Promise) {
   return Promise.all([
       knex('users').where('id','IN', [1,2,3,4,5]).del(),
       knex('xref_user_emails').where('email','IN',['alexei.ledenev@hpe.com', 'alexei.led@gmail.com']).del(),
-      knex('xref_user_emails').where('email','IN',['gaiaadmservice@gmail.com']).del(),
-//      knex('xref_user_emails').where('email','IN',['richard.plotkin@toptal.com', 'richardjplotkin@gmail.com','richard@richardplotkin.com','emilykplotkin@gmail.com']),
+      knex('xref_user_emails').where('email','IN',['gaiaadmservice@gmail.com']).del()
     ])
     .then(function () {
       return Promise.all([
@@ -17,18 +16,6 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({id: 1, firstName: 'Alexei', lastName: 'Ledenev', isSuperuser: true, isAdmin: true}),
         knex('users').insert({id: 10, firstName: 'Gaia', lastName: 'Team', isSuperuser: true, isAdmin: true}),
 
-        //USER 42 is a SUPERUSER
-//        knex('users').insert({id: 2, firstName: 'Awesome', lastName: 'Superuser', isSuperuser: true, isAdmin: true}),
-
-        //USER 43 is a SITE ADMIN
-//        knex('users').insert({id: 3, firstName: 'Site', lastName: 'Admin', isSuperuser: false, isAdmin: true}),
-
-        //USER 44 is an ACCOUNT ADMIN on account 36 and an ANALYST on account 37
-//        knex('users').insert({id: 4, firstName: 'Account', lastName: 'Admin', isSuperuser: false, isAdmin: false}),
-
-        //USER 45 is an ANALYST on account 36 and a MEMBER on account 37
-//        knex('users').insert({id: 5, firstName: 'Account', lastName: 'Analyst', isSuperuser: false, isAdmin: false})
-
       ]).then(function() {
         return Promise.all([
 
@@ -36,12 +23,8 @@ exports.seed = function(knex, Promise) {
           knex('xref_user_emails').insert({user_id:1,email:'alexei.ledenev@hpe.com'}),
           knex('xref_user_emails').insert({user_id:1,email:'alexei.led@gmail.com'}),
 
-          knex('xref_user_emails').insert({user_id:10,email:'gaiaadmservice@gmail.com'}),
+          knex('xref_user_emails').insert({user_id:10,email:'gaiaadmservice@gmail.com'})
 
-//          knex('xref_user_emails').insert({user_id:2,email:'richard.plotkin@toptal.com'}),
-//          knex('xref_user_emails').insert({user_id:3,email:'richardjplotkin@gmail.com'}),
-//          knex('xref_user_emails').insert({user_id:4,email:'richard@richardplotkin.com'}),
-//          knex('xref_user_emails').insert({user_id:5,email:'emilykplotkin@gmail.com'})
         ]);
       })
     });
