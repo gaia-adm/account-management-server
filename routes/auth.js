65ec6c829f2996429f4c821c3ba86372793e22ef
boriska70@gmail.com <boriska70@gmail.com>
2016-11-15 11:24:13 +0200

    API for  Gaia logout

--- routes/auth.js
+++ routes/auth.js
@@ -17,108 +17,117 @@ const ERRORS = require('./errors');
 const _ = require('lodash');
 const CONSTANTS = require('../config/constants');
 
-const validateUser      = function(req, accessToken, refreshToken, profile, next) {
-  // console.info('profile', profile);
-  if(!profile || !profile.emails) {
-    return Promise.reject();
-  }
-  let emails = profile.emails.map(function(email) {
-    return email.value;
-  });
-  //TODO: if there are multiple emails in the profile, with different user-ids, merge down to a single user-id
-  let lookupUserId = UserEmail.query().select('user_id').whereIn('email', emails).groupBy('user_id');
-  let user = lookupUserId.then(function(results) {
-    if(!results || results.length === 0) return Promise.reject(CONSTANTS.USER_NOT_FOUND);
-    if(results.length > 1) {
-      console.error('Emails match multiple user IDs');
+const validateUser = function (req, accessToken, refreshToken, profile, next) {
+    // console.info('profile', profile);
+    if (!profile || !profile.emails) {
+        return Promise.reject();
     }
-    let userId = results[0].user_id;
-    return User.where({id: userId}).fetch();
-  });
-  return user;
-};
-
-const validationCallback = function(fn, req, accessToken, refreshToken, profile, next) {
-  fn(req, accessToken, refreshToken, profile, next)
-    .then(function(result) {
-      // console.info('result', result);
-      return next(null, result);
-    })
-    .catch(function(error) {
-      // console.info('error', error);
-      if(_.isString(error)) {
-        var e = new Error(error);
-        e.status = 400;
-        return next(e);
-      }
-      return next(error);
+    let emails = profile.emails.map(function (email) {
+        return email.value;
     });
+    //TODO: if there are multiple emails in the profile, with different user-ids, merge down to a single user-id
+    let lookupUserId = UserEmail.query().select('user_id').whereIn('email', emails).groupBy('user_id');
+    let user = lookupUserId.then(function (results) {
+        if (!results || results.length === 0) return Promise.reject(CONSTANTS.USER_NOT_FOUND);
+        if (results.length > 1) {
+            console.error('Emails match multiple user IDs');
+        }
+        let userId = results[0].user_id;
+        return User.where({id: userId}).fetch();
+    });
+    return user;
 };
 
-const validateUserCallback = function(req, accessToken, refreshToken, profile, next) {
-  return validationCallback(validateUser, req, accessToken, refreshToken, profile, next);
+const validationCallback = function (fn, req, accessToken, refreshToken, profile, next) {
+    fn(req, accessToken, refreshToken, profile, next)
+        .then(function (result) {
+            // console.info('result', result);
+            return next(null, result);
+        })
+        .catch(function (error) {
+            // console.info('error', error);
+            if (_.isString(error)) {
+                var e = new Error(error);
+                e.status = 400;
+                return next(e);
+            }
+            return next(error);
+        });
 };
 
-const resolveUserJWT = function(req, res) {
-  // console.info('resolveUserJWT', req, res);
-  let user = req.user;
-  let token = jwt.sign({id: user.id}, config.get('secret'), {
-    expiresIn: '365d'
-  });
-  let myDomain = req.get('Host');
-    if(req.get('Host').startsWith('acmc')){
+const validateUserCallback = function (req, accessToken, refreshToken, profile, next) {
+    return validationCallback(validateUser, req, accessToken, refreshToken, profile, next);
+};
+
+const resolveUserJWT = function (req, res) {
+    // console.info('resolveUserJWT', req, res);
+    let user = req.user;
+    let token = jwt.sign({id: user.id}, config.get('secret'), {
+        expiresIn: '365d'
+    });
+    let myDomain = req.get('Host');
+    if (req.get('Host').startsWith('acmc')) {
         myDomain = myDomain.substring(4); //set cookie for entire Gaia environment domain instead of acmc subdomain
     }
-  res.cookie('gaia.token', token, {
-    httpOnly: true,
-      domain: myDomain
-  });
-  res.json({ success: true, user: user});
+    res.cookie('gaia.token', token, {
+        httpOnly: true,
+        domain: myDomain
+    });
+    res.json({success: true, user: user});
 };
 
 //passport setup
 passport.use('google', new GoogleStrategy({
-    clientID: config.get('authentication.googleStrategy.clientId'),
-    clientSecret: config.get('authentication.googleStrategy.clientSecret'),
-    callbackURL: config.get('authentication.googleStrategy.callbackURL'),
-  },
-    new function LogMyClientForEasierDebugging(){
-      console.log('My Client ID is ' + config.get('authentication.googleStrategy.clientId').substring(0,8))
+        clientID: config.get('authentication.googleStrategy.clientId'),
+        clientSecret: config.get('authentication.googleStrategy.clientSecret'),
+        callbackURL: config.get('authentication.googleStrategy.callbackURL'),
     },
-  validateUserCallback)
+    new function LogMyClientForEasierDebugging() {
+        console.log('My Client ID is ' + config.get('authentication.googleStrategy.clientId').substring(0, 8))
+    },
+    validateUserCallback)
 );
 
 passport.use('google-id-token', new GoogleTokenStrategy({
-    clientID: config.get('authentication.googleStrategy.clientId')
-  },
-  function(parsedToken, googleId, done) {
-    let profile = _.clone(parsedToken.payload);
-    profile.emails = [{value: profile.email}];
-    profile.displayName = profile.name;
-    return validateUserCallback(null, null, null, profile, done);
-  })
+        clientID: config.get('authentication.googleStrategy.clientId')
+    },
+    function (parsedToken, googleId, done) {
+        let profile = _.clone(parsedToken.payload);
+        profile.emails = [{value: profile.email}];
+        profile.displayName = profile.name;
+        return validateUserCallback(null, null, null, profile, done);
+    })
 );
 
 /* GET users listing. */
 router.get('/google',
-  passport.authenticate('google', {
-    scope: 'profile email',
-    response_type: 'token'
-  }));
+    passport.authenticate('google', {
+        scope: 'profile email',
+        response_type: 'token'
+    }));
 
 router.get('/google/return',
-  passport.authenticate('google', {
-    session: false,
-    failureRedirect: '/login',
-    passReqToCallback: true,
-  }), resolveUserJWT);
+    passport.authenticate('google', {
+        session: false,
+        failureRedirect: '/login',
+        passReqToCallback: true,
+    }), resolveUserJWT);
 
 router.post('/google/token',
-  passport.authenticate('google-id-token', {
-    session: false,
-    passReqToCallback: true
-  }),
-  resolveUserJWT);
+    passport.authenticate('google-id-token', {
+        session: false,
+        passReqToCallback: true
+    }),
+    resolveUserJWT);
+
+router.get('/gaia.logout', function (req, res) {
+    res.clearCookie('gaia.token', {
+        httpOnly: true,
+        domain: '.' + req.hostname
+    });
+    res.json({success: true});
+});
+
 
 exports.router = router;
 exports.validateUser = validateUser;
